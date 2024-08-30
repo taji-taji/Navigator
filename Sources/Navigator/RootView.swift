617adea59260ee7738273076638728d70c83cf86
taji-taji <y.tajika529@gmail.com>
2024-08-30 19:24:56 +0900

    add documentation

--- Sources/Navigator/RootView.swift
+++ Sources/Navigator/RootView.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
+/// The View that wraps the initial View of the Navigation.
 public struct RootView<Content>: View where Content: View {
     @StateObject var navigator: Navigator
     private let content: () -> Content
-
+    
+    ///
+    /// - Parameters:
+    ///   - viewProvider: A Closure that resolves the specific View type corresponding to the destination identifier.
+    ///   - content: The wrapped View.
     public init(
         @ViewBuilder viewProvider: @escaping (any NavigationDestination) -> some View,
         @ViewBuilder content: @escaping () -> Content
@@ -12,6 +17,7 @@ public struct RootView<Content>: View where Content: View {
         self._navigator = StateObject(wrappedValue: Navigator(viewProvider))
     }
 
+    /// The content and behavior of the view.
     public var body: some View {
         NavigationStack(path: $navigator.path) {
             content()
